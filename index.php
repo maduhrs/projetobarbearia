8ad6176b6ba0064c41093d1f39f44cd94e77a012
loanac <lcavalcantis07@gmail.com>
2020-10-13 11:24:42 -0300

    primeirocommit

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="public/css/bootstrap/css/bootstrap.css">
+
+
+    <!-- meu estilo css -->
+    <link rel="stylesheet" href="public/css/meuestilo.css">
+    <link rel="stylesheet" href="public/css/meuestilo2.css">
+
+    <!-- icomoon -->
+    <link rel="stylesheet" href="public/css/style.css">
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="public/css/fonts/fontawesome-free-5.14.0-web/css/all.css">
+
+
+    <title>Barbearia JoJo</title>
+</head>
+
+<body>
+
+    <div class="sec">
+        <div class="containerzero container-fluid">
+            <header>
+                <nav id="menu">
+                    <!-- redes sociais-->
+                    <ul class="social">
+                        <li><a href="#"><i class="fab fa-facebook-f m-1"></i></a></li>
+                        <li><a href="#"><i class="fab fa-instagram m-1"></i></a></li>
+                        <li><a href="#"><i class="fab fa-whatsapp m-1"></i></a></li>
+                        <li><a href="#"><i class="fab fa-twitter m-1"></i></i></a></li>
+                    </ul>
+                    <!-- redes sociais fim -->
+                    <div class="mr-auto"></div>
+                    <ul class="nav">
+                        <li class="float-right m-1"><a href="#">início</a></li>
+                        <li class="float-right m-1"><a href="#section2">sobre</a></li>
+                        <li class="float-right m-1 "><a href="#section3">serviços</a></li>
+                        <li class="float-right m-1"><a href="#section4">agendamento</a></li>
+                        <li class="float-right m-1"><a href="#section5">contatos</a></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <div class="content">
+                <div class="textbox">
+                    <h2>Barbearia
+                        <br><span>do Jotaro</span></h2> 
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt, dolorum laborum iusto, et porro excepturi, temporibus reprehenderit natus molestiae ab ducimus! Culpa earum, rerum hic maiores libero dicta. Ipsa, veritatis.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="custom-shape-divider-bottom-1602519020">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <img class="icon1" src="public/img/barber.png" alt="">
+
+    </div>
+
+
+    <!--------------------------------- SOBRE ----------------------------------------->
+    <div class="container1" id="section2">
+        <div class="box1">
+
+            <h3 class="text-center text-light">SOBRE</h3>
+            <div class="row">
+                <div class="col-md-6">
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="public/img/jotarok.jpg" alt="Segundo Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="public/img/curso-barbeiro-2.png" alt="Terceiro Slide">
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100 " src="public/img/reforma-de-barbearia.jpg" alt="Terceiro Slide">
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Anterior</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Próximo</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <p class="text-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. At molestiae quam, vitae dolor itaque nostrum porro, ex voluptatum alias natus, pariatur explicabo. Eaque ullam velit aperiam illo nemo unde dolor! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam enim vero nobis, expedita, numquam quisquam illo odit dolor consequuntur, odio ex quibusdam cumque libero. Laboriosam corporis officia rem at similique? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos modi earum omnis ex sequi quae, neque eum aperiam quas facere id culpa illo atque maiores cum repellendus porro. Consectetur, laudantium!</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+    <!-------------------------- SERVICOS ---------------------- -->
+    <div class="container2" id="section3">
+        <div class="custom-shape-divider-top-1602370345">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+            </svg>
+        </div>
+        <div class="box2">
+            <h3 class="text-center">SERVIÇOS</h3>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="width: 12rem;">
+                        <img class="card-img-top" style="width: 9em;" src="public/img/adesivo1.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Corte e Barba</h5>
+                            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                            <a href="#section4" class="btn btn-warning">Agendar</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style="width: 12rem;">
+                        <img class="card-img-top" style="width: 9em;" src="public/img/creme.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Química Capilar</h5>
+                            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                            <a href="#section4" class="btn btn-warning">Agendar</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style="width: 12rem;">
+                        <img class="card-img-top " style="width: 9em;" src="public/img/groom.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Dia do Noivo</h5>
+                            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                            <a href="#section4" class="btn btn-warning">Agendar</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col">
+                    <div class="card" style="width: 12rem;">
+                        <img class="card-img-top " style="width: 9em; justify-content: center;" src="public/img/massage.png" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Massagem</h5>
+                            <p class="card-text">Um exemplo de texto rápido para construir o título do card e fazer preencher o conteúdo do card.</p>
+                            <a href="#section4" class="btn btn-warning">Agendar</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+    <!-------------------------------- AGENDAMENTO ------------------------------------->
+    <div class="container3" id="section4">
+        <div class="box3">
+            <br>
+            <h3 class="text-center">AGENDAMENTO</h3>
+            <br>
+            <!-- form -->
+            <form>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><strong>Nome</strong></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Digite seu nome">
+                            <small id="emailHelp" class="form-text text-muted"></small>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1"><strong>Celular</strong></label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="">
+                            <small id="emailHelp" class="form-text text-muted"></small>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row ">
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1"><strong>Turno</strong></label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Manhã</option>
+                                <option>Tarde</option>
+                                <option>Noite</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1"><strong>Horário</strong></label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>8h</option>
+                                <option>10h</option>
+                                <option>13h</option>
+                                <option>14h</option>
+                                <option>15h</option>
+                                <option>18h</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1"><strong>Local</strong></label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                                <option>Unidade Recife</option>
+                                <option>Unidade São Paulo</option>
+                                <option>Unidade Rio de Janeiro</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="form-group row">
+                            <div class="col-sm-2"><strong>Serviço:</strong></div>
+                            <div class="col-sm-10">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Corte
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Barba
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Química capilar
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Dia do noivo
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck1">
+                                    <label class="form-check-label" for="gridCheck1">
+                                        Massagem
+                                    </label>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group text-right">
+
+                    <button type="submit" class="btn btn-warning">Confirmar</button>
+                </div>
+            </form>
+            <!-- end form -->
+        </div>
+    </div>
+
+    <!-- --------------------- CONTATO ------------------------- -->
+    <div class="container4">
+        <div class="box4" id="section5">
+            <br>
+            <h3 class="text-center text-light">CONTATOS</h3>
+
+            <div class="conteudo">
+                <h2 class="text-center">Unidades</h2>
+                <br>
+                <p class="text-center"><strong>Recife</strong>- Boa Viagem, Rua Crusaders, nº 03</p>
+                <p class="text-center"><i class="lua-phone"></i> 4002-8922</p>
+                <hr>
+                <p class="text-center"><strong>São Paulo</strong>- Centro, Rua Jolyne, nº 06</p>
+                <p class="text-center"><i class="lua-phone"></i> 4004-8722</p>
+                <hr>
+                <p class="text-center"><strong>Rio de Janeiro</strong>- Copacabana, Rua Star Platinum, nº 17</p>
+                <p class="text-center"><i class="lua-phone"></i> 4002-8222</p>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- --------------- FOOTER ------------------ -->
+    <footer>
+        <div class="container5">
+
+            <div class="row">
+                <div class="col text-center py-3">
+                    <ul class="list-group list-group-flush noBackground">
+                        <h1 class="text-light">Unidades</h1>
+                        <li class="list-group-item noBackground ">Recife</li>
+                        <li class="list-group-item noBackground">São Paulo</li>
+                        <li class="list-group-item noBackground">Rio de Janeiro</li>
+                    </ul>
+                </div>
+
+                <div class="col text-center py-4">
+                    <img class="rounded-circle w-50 img-fluid" src="public/img/logoheader.png" alt="">
+                </div>
+
+                <div class="col text-center py-3">
+                    <ul class="list-group list-group-flush noBackground">
+                        <h1 class="text-light">Redes Sociais</h1>
+                        <li class="list-group-item noBackground"><i class="lua-facebook"></i> BarberJotaro</li>
+                        <li class="list-group-item noBackground"><i class="lua-instagram"></i> @BarberJoJo</li>
+                        <li class="list-group-item noBackground"><i class="lua-youtube"></i> BarberJOJO</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <!-- jQuery -->
+    <script src="public/js/jquery.js"></script>
+    <script src="public/css/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="public/css/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
